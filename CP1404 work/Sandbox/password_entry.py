97e78003588b8616bd343a2f660cb52ebc305127
Jean-Luc Andreassen <Jean-Luc2000@hotmail.com>
2019-08-22 12:00:48 +1000

    Add password check program

--- CP1404 work/Sandbox/password_entry.py
+++ CP1404 work/Sandbox/password_entry.py
@@ -1,1 +1,14 @@
 """Jean-Luc Andreassen"""
+
+MINIMUM_LENGTH = 6
+
+
+def main():
+    password = input("Please enter a password of at least 6 letters: ")
+    while len(password) < MINIMUM_LENGTH:
+        password = input("That password is invalid, please enter at least 6 letters: ")
+
+    print("*"*len(password))
+
+
+main()
